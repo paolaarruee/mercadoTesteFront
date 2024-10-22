a2668af52efe58bdc7409d1acce0450c73ff0e6d
Paola <paolaarruee@gmail.com>
2024-10-21 21:11:31 -0300

    Ajuste menu

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,18 +1,33 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { LoginService } from '../../services/login.service';
+import { ToastService } from '../../services/toast.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   @ViewChild('menuNav', { static: true })
   menuNavRef!: ElementRef;
+  isHomePage: boolean = false;
 
-  public constructor(private loginService: LoginService) {}
+  public constructor(
+    private loginService: LoginService,
+    private toastService: ToastService,
+    private router: Router
+  ) {}
 
-  public toggleMenu() {
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isHomePage = this.router.url === '/';
+      }
+    });
+  }
+
+  public toggleMenu(): void {
     const menuNav = this.menuNavRef.nativeElement;
     if (menuNav) {
       menuNav.classList.toggle('active');
@@ -21,6 +36,8 @@ export class HeaderComponent {
 
   public logout(): void {
     this.loginService.logout();
+    this.toastService.showMessage('Deslogado com sucesso!');
+    this.router.navigate(['/']);
   }
 
   public get isAuthenticated(): boolean {
